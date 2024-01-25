dfc461aacbeec15402811b1d43f0e6293f3c2e68
Gabriel Cardiano <gabriel.cardiano1@gmail.com>
2024-01-25 14:02:15 -0300

    feat: req 5 - cria métodos cadastrarPessoaCandidata e cadastrarPessoaEleitora

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 /**
  * The type Gerenciamento votacao.
  */
-public abstract class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
+public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
 
   private ArrayList<PessoaCandidata> pessoasCandidatas;
   private ArrayList<PessoaEleitora> pessoasEleitoras;
@@ -15,9 +15,42 @@ public abstract class GerenciamentoVotacao implements GerenciamentoVotacaoInterf
    * Instantiates a new Gerenciamento votacao.
    */
   public GerenciamentoVotacao() {
-    this.pessoasCandidatas = new ArrayList<PessoaCandidata>();
-    this.pessoasEleitoras = new ArrayList<PessoaEleitora>();
-    this.cpfsComputados = new ArrayList<String>();
+    this.pessoasCandidatas = new ArrayList<>();
+    this.pessoasEleitoras = new ArrayList<>();
+    this.cpfsComputados = new ArrayList<>();
   }
-  
+
+  @Override
+  public void cadastrarPessoaCandidata(String nome, int numero) {
+    for (PessoaCandidata candidato : this.pessoasCandidatas) {
+      if (candidato.getNumero() == numero) {
+        System.out.println("Número da pessoa candidata já utilizado!");
+        return;
+      }
+    }
+    PessoaCandidata novoCandidato = new PessoaCandidata(nome, numero);
+    this.pessoasCandidatas.add(novoCandidato);
+  }
+
+  @Override
+  public void cadastrarPessoaEleitora(String nome, String cpf) {
+    for (PessoaEleitora eleitor : this.pessoasEleitoras) {
+      if (eleitor.getCpf() == cpf) {
+        System.out.println("Pessoa eleitora já cadastrada!");
+        return;
+      }
+    }
+    PessoaEleitora novoEleitor = new PessoaEleitora(nome, cpf);
+    this.pessoasEleitoras.add(novoEleitor);
+  }
+
+  @Override
+  public void votar(String cpfPessoaEleitora, int numeroPessoaCandidata) {
+
+  }
+
+  @Override
+  public void mostrarResultado() {
+
+  }
 }
