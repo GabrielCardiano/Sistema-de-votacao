cdbec56fd9caff5138d64397b317bded24139569
Gabriel Cardiano <gabriel.cardiano1@gmail.com>
2024-01-25 00:36:35 -0300

    feat: req 4 - cria atributos da classe GerenciamentoVotacaoa

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -1,8 +1,23 @@
 package com.betrybe.sistemadevotacao;
 
+import java.util.ArrayList;
+
 /**
  * The type Gerenciamento votacao.
  */
-public class GerenciamentoVotacao {
+public abstract class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
 
+  private ArrayList<PessoaCandidata> pessoasCandidatas;
+  private ArrayList<PessoaEleitora> pessoasEleitoras;
+  private ArrayList<String> cpfsComputados;
+
+  /**
+   * Instantiates a new Gerenciamento votacao.
+   */
+  public GerenciamentoVotacao() {
+    this.pessoasCandidatas = new ArrayList<PessoaCandidata>();
+    this.pessoasEleitoras = new ArrayList<PessoaEleitora>();
+    this.cpfsComputados = new ArrayList<String>();
+  }
+  
 }
