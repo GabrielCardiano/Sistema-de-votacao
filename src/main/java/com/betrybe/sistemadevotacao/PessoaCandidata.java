70a370c15333bf31e2b68f60dee1cab87901348b
Gabriel Cardiano <gabriel.cardiano1@gmail.com>
2024-01-24 23:11:25 -0300

    feat: req 2 - cria a classe PessoaCandidata

--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -3,6 +3,37 @@ package com.betrybe.sistemadevotacao;
 /**
  * The type Pessoa candidata.
  */
-public class PessoaCandidata {
+public class PessoaCandidata extends Pessoa {
 
+  private int numero;
+  private int votos;
+
+  /**
+   * Instantiates a new Pessoa candidata.
+   *
+   * @param nome    the nome
+   * @param numeros the numeros
+   */
+  public PessoaCandidata(String nome, int numeros) {
+    this.nome = nome;
+    this.numero = numeros;
+    this.votos = 0;
+  }
+
+  public int receberVoto() {
+    return this.votos += 1;
+  }
+
+  //  getters e setters
+  public int getNumero() {
+    return numero;
+  }
+
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  public int getVotos() {
+    return votos;
+  }
 }
